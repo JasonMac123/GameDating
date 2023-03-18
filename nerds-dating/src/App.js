cfaf2abe84687ea4d747ed1f9a8af35f83528788
Chris Baltis <cbaltis@gmail.com>
2023-03-18 13:41:17 +0000

    done basic rendering for login and register

--- nerds-dating/src/App.js
+++ nerds-dating/src/App.js
@@ -1,20 +1,22 @@
 import axios from "axios";
+import Login from './components/Login'
+import Register from "./components/Register";
 
 function App() {
-  axios
-    .get("/api/users")
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((e) => console.log(e));
+  // axios
+  //   .get("/api/users")
+  //   .then((res) => {
+  //     console.log(res.data);
+  //   })
+  //   .catch((e) => console.log(e));
 
-  axios.post("/api/users", { username: "hello" }).then((data) => {
-    console.log(data);
-  });
+  // axios.post("/api/users", { username: "hello" }).then((data) => {
+  //   console.log(data);
+  // });
 
   return (
     <div className="text-sky-400 text-2xl underline decoration-solid">
-      
+      {<Register />}
     </div>
   );
 }
--- nerds-dating/src/components/Register.js
+++ nerds-dating/src/components/Register.js
@@ -1,39 +1,51 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-function Signup() {
+function Register() {
 
 const [name, setName]=useState('');
 const [email, setEmail]=useState('');
 const [phoneNumber, setPhoneNumber]=useState('');
 const [password, setPassword]=useState('');
+const [profilePicture, setProfilePicture]=useState('');
+const [coverPicture, setCoverPicture]=useState('');
+const [genderIdentity, setGenderIdentity]=useState('');
+const [summary, setSummary]=useState('');
 
-async function submit(e){
-e.preventDefault();
 
-try{
-await axios.post("http://localhost:3000/", {
-name, email, phoneNumber, password
-})
-}
-catch(e){
-console.log(e)
-}
-}
+
 
 return (
 <div classname="signup">
-<h1>Signup</h1>
+<h1>Register</h1>
 
-<form action="/login" method="POST">
-<input type="text" onChange={(e)=>{setName(e.target.value)}} placeholder="Name"/>
-<input type="email" onChange={(e)=>{setEmail(e.target.value)}} placeholder="Email"/>
-<input type="text" onChange={(e)=>{setPhoneNumber(e.target.value)}} placeholder="Phone-Number"/>
-<input type="password" onChange={(e)=>{setPassword(e.target.value)}} placeholder="Name"/>
-<input type="submit" onClick={submit}/>
+<form action="/" method="POST">
+  <input type="text" onChange={(e)=>{setName(e.target.value)}} placeholder="Name"/>
+  <input type="email" onChange={(e)=>{setEmail(e.target.value)}} placeholder="Email"/>
+  <input type="password" onChange={(e)=>{setPassword(e.target.value)}} placeholder="Password"/>
+  <input type="text" onChange={(e)=>{setPhoneNumber(e.target.value)}} placeholder="Phone-Number"/>
+  <input type="text" onChange={(e)=>{setProfilePicture(e.target.value)}} placeholder="Profile Picture"/>
+  <input type="text" onChange={(e)=>{setCoverPicture(e.target.value)}} placeholder="Cover Picture"/>
+  <fieldset>
+    <legend>Please select your gender preference:</legend>
+    <div>
+      <input type="radio" id="female" name="gender_preference" value="female" onClick={(e)=>{setGenderIdentity(e.target.value)}}/>
+      <label for="female">Female</label><br/>
+      <input type="radio" id="male" name="gender_preference" value="male" onClick={(e)=>{setGenderIdentity(e.target.value)}}/>
+      <label for="male">Male</label><br/>
+      <input type="radio" id="both" name="gender_preference" value="both" onClick={(e)=>{setGenderIdentity(e.target.value)}}/>
+      <label for="both">Both</label>
+    </div>
+  </fieldset>
+  <input type="text" onChange={(e)=>{setSummary(e.target.value)}} placeholder="Summary"/>
+  <div>
+      <button type="submit">Submit</button>
+  </div>
 </form>
 </div>
 );
 }
 
-export default Signup;
+export default Register;
+
+// (name, email, password, phone_number, profile_picture, cover_picture, gender_identity, gender_preference, summary)
