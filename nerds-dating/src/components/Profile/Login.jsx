eceea68fa57a5af8f8770a6a9d99172c4bc893e5
JasonSnow123 <m.jason.2018@gmail.com>
2023-03-22 17:17:42 -0400

    fix class and for errors in react

--- nerds-dating/src/components/Profile/Login.jsx
+++ nerds-dating/src/components/Profile/Login.jsx
@@ -30,35 +30,38 @@ function Login(props) {
   };
 
   return (
-    <section class="gradient-form h-screen bg-gradient-to-br from-amber-200 via-violet-600 to-sky-900 dark:bg-neutral-700">
-      <div class="container h-full p-10">
-        <div class="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
-          <div class="w-full">
-            <div class="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
-              <div class="g-0 lg:flex lg:flex-wrap">
-                <div class="px-4 md:px-0 lg:w-6/12">
-                  <div class="">
-                    <div class="text-center">
+    <section className="gradient-form h-screen bg-gradient-to-br from-amber-200 via-violet-600 to-sky-900 dark:bg-neutral-700">
+      <div className="container h-full p-10">
+        <div className="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
+          <div className="w-full">
+            <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
+              <div className="g-0 lg:flex lg:flex-wrap">
+                <div className="px-4 md:px-0 lg:w-6/12">
+                  <div className="">
+                    <div className="text-center">
                       <img
-                        class="mx-auto w-48"
+                        className="mx-auto w-48"
                         src="https://cdn-icons-png.flaticon.com/512/188/188932.png"
                         alt="logo"
                       />
-                      <h4 class="mt-1 mb-12 pb-1 text-xl font-semibold">
+                      <h4 className="mt-1 mb-12 pb-1 text-xl font-semibold">
                         Gamers Only
                       </h4>
                     </div>
-                    <div class="bg-gradient-to-l from-purple-200 via-purple-400 to-purple-800 rounded">
+                    <div className="bg-gradient-to-l from-purple-200 via-purple-400 to-purple-800 rounded">
                       <form
                         autoComplete="off"
                         onSubmit={handleSubmit}
-                        class="md:mx-6 md:p-12"
+                        className="md:mx-6 md:p-12"
                       >
-                        <p class="mb-4">Please login to your account:</p>
-                        <div class="relative mb-4" data-te-input-wrapper-init>
+                        <p className="mb-4">Please login to your account:</p>
+                        <div
+                          className="relative mb-4"
+                          data-te-input-wrapper-init
+                        >
                           <input
                             type="email"
-                            class="peer block min-h-[auto] bg-white w-full rounded bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-red-600 transition-all duration-200 ease-linear"
+                            className="peer block min-h-[auto] bg-white w-full rounded bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-red-600 transition-all duration-200 ease-linear"
                             id="email"
                             value={user.email}
                             name="email"
@@ -66,10 +69,13 @@ function Login(props) {
                             placeholder="Email"
                           />
                         </div>
-                        <div class="relative mb-4" data-te-input-wrapper-init>
+                        <div
+                          className="relative mb-4"
+                          data-te-input-wrapper-init
+                        >
                           <input
                             type="password"
-                            class="peer block min-h-[auto] bg-white w-full rounded bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-red-600 transition-all duration-200 ease-linear"
+                            className="peer block min-h-[auto] bg-white w-full rounded bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-red-600 transition-all duration-200 ease-linear"
                             id="password"
                             value={user.password}
                             name="password"
@@ -77,10 +83,10 @@ function Login(props) {
                             placeholder="Password"
                           />
                         </div>
-                        <div class="mb-12 pt-1 pb-1 text-center">
+                        <div className="mb-12 pt-1 pb-1 text-center">
                           {/* <input type="submit" onClick={handleSubmit} /> */}
                           <button
-                            class="mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
+                            className="mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
                             type="submit"
                             onClick={handleSubmit}
                             data-te-ripple-init
@@ -93,11 +99,11 @@ function Login(props) {
                             Login
                           </button>
                         </div>
-                        <div class="flex items-center justify-between pb-6">
-                          <p class="mb-0 mr-2">Don't have an account?</p>
+                        <div className="flex items-center justify-between pb-6">
+                          <p className="mb-0 mr-2">Don't have an account?</p>
                           <button
                             type="button"
-                            class="mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
+                            className="mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
                             data-te-ripple-init
                             data-te-ripple-color="light"
                             onClick={handleRegister}
@@ -114,17 +120,17 @@ function Login(props) {
                   </div>
                 </div>
                 <div
-                  class="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
+                  className="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
                   style={{
                     background:
                       "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
                   }}
                 >
-                  <div class="px-4 py-6 text-white md:mx-6 md:p-12">
-                    <h4 class="mb-6 text-xl font-semibold">
+                  <div className="px-4 py-6 text-white md:mx-6 md:p-12">
+                    <h4 className="mb-6 text-xl font-semibold">
                       A place for gamers to find true love
                     </h4>
-                    <p class="text-sm">
+                    <p className="text-sm">
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit,
                       sed do eiusmod tempor incididunt ut labore et dolore magna
                       aliqua. Ut enim ad minim veniam, quis nostrud exercitation
--- nerds-dating/src/components/Profile/Register.jsx
+++ nerds-dating/src/components/Profile/Register.jsx
@@ -29,46 +29,42 @@ function Register(props) {
   const handleLogin = (event) => {
     event.preventDefault();
     props.setDisplay(1);
-  }
+  };
 
   return (
-    <section class="gradient-form h-full bg-black dark:bg-neutral-700">
-      <div class="container h-full p-10">
-        <div
-          class="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
-          <div class="w-full">
-            <div
-              class="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
-              <div class="g-0 lg:flex lg:flex-wrap bg-gradient-to-r from-pink-400 to-pink-600">
-                <div class="px-4 md:px-0 lg:w-6/12">
-                  <div class="md:mx-6 md:p-12">
-                    <div class="text-center">
+    <section className="gradient-form h-full bg-black dark:bg-neutral-700">
+      <div className="container h-full p-10">
+        <div className="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
+          <div className="w-full">
+            <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
+              <div className="g-0 lg:flex lg:flex-wrap bg-gradient-to-r from-pink-400 to-pink-600">
+                <div className="px-4 md:px-0 lg:w-6/12">
+                  <div className="md:mx-6 md:p-12">
+                    <div className="text-center">
                       <img
-                        class="mx-auto w-48"
+                        className="mx-auto w-48"
                         src="https://res.cloudinary.com/teepublic/image/private/s--Mlyx9Zg1--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_000000,e_outline:48/co_000000,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/t_watermark_lock/c_limit,f_auto,h_630,q_90,w_630/v1611931472/production/designs/18991833_0.jpg"
-                        alt="logo" />
+                        alt="logo"
+                      />
                       <h1>Gamers Only</h1>
                     </div>
 
                     <form autoComplete="off" onSubmit={handleSubmit}>
-                      <p class="mb-4">Please register an account:</p>
-                      <div class="relative mb-4" data-te-input-wrapper-init>
+                      <p className="mb-4">Please register an account:</p>
+                      <div className="relative mb-4" data-te-input-wrapper-init>
                         <input
                           type="text"
-                          class="peer block min-h-[auto] bg-white w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
+                          className="peer block min-h-[auto] bg-white w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
                           value={user.name}
                           name="name"
                           onChange={handleChange}
                           placeholder="Name"
-
                         />
                       </div>
-                      <div class="relative mb-4" data-te-input-wrapper-init>
-
+                      <div className="relative mb-4" data-te-input-wrapper-init>
                         <input
                           type="email"
-                          class="peer block min-h-[auto] bg-white w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
-
+                          className="peer block min-h-[auto] bg-white w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
                           value={user.email}
                           name="email"
                           onChange={handleChange}
@@ -76,13 +72,10 @@ function Register(props) {
                         />
                       </div>
 
-                      <div class="relative mb-4" data-te-input-wrapper-init>
-
-
+                      <div className="relative mb-4" data-te-input-wrapper-init>
                         <input
                           type="password"
-                          class="peer block min-h-[auto] w-full bg-white rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
-
+                          className="peer block min-h-[auto] w-full bg-white rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
                           value={user.password}
                           name="password"
                           onChange={handleChange}
@@ -90,12 +83,10 @@ function Register(props) {
                         />
                       </div>
 
-                      <div class="relative mb-4" data-te-input-wrapper-init>
-
+                      <div className="relative mb-4" data-te-input-wrapper-init>
                         <input
                           type="text"
-                          class="peer block min-h-[auto] w-full bg-white rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
-
+                          className="peer block min-h-[auto] w-full bg-white rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
                           value={user.phone_number}
                           name="phone_number"
                           onChange={handleChange}
@@ -103,12 +94,10 @@ function Register(props) {
                         />
                       </div>
 
-                      <div class="relative mb-4" data-te-input-wrapper-init>
-
+                      <div className="relative mb-4" data-te-input-wrapper-init>
                         <input
                           type="text"
-                          class="peer block min-h-[auto] w-full bg-white rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
-
+                          className="peer block min-h-[auto] w-full bg-white rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
                           value={user.profile_picture}
                           name="profile_picture"
                           onChange={handleChange}
@@ -116,12 +105,10 @@ function Register(props) {
                         />
                       </div>
 
-                      <div class="relative mb-4" data-te-input-wrapper-init>
-
+                      <div className="relative mb-4" data-te-input-wrapper-init>
                         <input
                           type="text"
-                          class="peer block min-h-[auto] w-full bg-white rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
-
+                          className="peer block min-h-[auto] w-full bg-white rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
                           value={user.cover_picture}
                           name="cover_picture"
                           onChange={handleChange}
@@ -129,10 +116,8 @@ function Register(props) {
                         />
                       </div>
 
-                      <div class="relative mb-4" data-te-input-wrapper-init>
-
-                        <fieldset class="peer block min-h-[auto] w-full bg-white rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
-                        >
+                      <div className="relative mb-4" data-te-input-wrapper-init>
+                        <fieldset className="peer block min-h-[auto] w-full bg-white rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear">
                           <h4>Please select your gender preference:</h4>
                           <div>
                             <input
@@ -142,7 +127,7 @@ function Register(props) {
                               value="female"
                               onClick={handleChange}
                             />
-                            <label for="female">Female</label>
+                            <label htmlFor="female">Female</label>
                             <br />
                             <input
                               type="radio"
@@ -151,7 +136,7 @@ function Register(props) {
                               value="male"
                               onClick={handleChange}
                             />
-                            <label for="male">Male</label>
+                            <label htmlFor="male">Male</label>
                             <br />
                             <input
                               type="radio"
@@ -160,14 +145,12 @@ function Register(props) {
                               value="both"
                               onClick={handleChange}
                             />
-                            <label for="both">Both</label>
+                            <label htmlFor="both">Both</label>
                           </div>
                         </fieldset>
                       </div>
-                      <div class="relative mb-4" data-te-input-wrapper-init>
-
-                        <fieldset class="peer block min-h-[auto] bg-white w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6]  transition-all duration-200 ease-linear"
-                        >
+                      <div className="relative mb-4" data-te-input-wrapper-init>
+                        <fieldset className="peer block min-h-[auto] bg-white w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6]  transition-all duration-200 ease-linear">
                           <h4>Please select your gender preference:</h4>
                           <div>
                             <input
@@ -177,7 +160,7 @@ function Register(props) {
                               value="female"
                               onClick={handleChange}
                             />
-                            <label for="female">Female</label>
+                            <label htmlFor="female">Female</label>
                             <br />
                             <input
                               type="radio"
@@ -186,7 +169,7 @@ function Register(props) {
                               value="male"
                               onClick={handleChange}
                             />
-                            <label for="male">Male</label>
+                            <label htmlFor="male">Male</label>
                             <br />
                             <input
                               type="radio"
@@ -195,17 +178,15 @@ function Register(props) {
                               value="both"
                               onClick={handleChange}
                             />
-                            <label for="both">Both</label>
+                            <label htmlFor="both">Both</label>
                           </div>
                         </fieldset>
                       </div>
 
-                      <div class="relative mb-4" data-te-input-wrapper-init>
-
+                      <div className="relative mb-4" data-te-input-wrapper-init>
                         <input
                           type="text"
-                          class="peer block min-h-[auto] w-full bg-white rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
-
+                          className="peer block min-h-[auto] w-full bg-white rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] transition-all duration-200 ease-linear"
                           value={user.summary}
                           name="summary"
                           onChange={handleChange}
@@ -213,26 +194,34 @@ function Register(props) {
                         />
                       </div>
 
-                      <div class="mb-12 pt-1 pb-1 text-center">
+                      <div className="mb-12 pt-1 pb-1 text-center">
                         <button
-                          class="mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
+                          className="mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
                           type="submit"
                           onClick={handleSubmit}
                           data-te-ripple-init
                           data-te-ripple-color="light"
-                          style={{ background: "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)" }}>
+                          style={{
+                            background:
+                              "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
+                          }}
+                        >
                           Sign up
                         </button>
                       </div>
-                      <div class="flex items-center justify-between pb-6">
-                        <p class="mb-0 mr-2">Have an account?</p>
+                      <div className="flex items-center justify-between pb-6">
+                        <p className="mb-0 mr-2">Have an account?</p>
                         <button
                           type="button"
-                          class="mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
+                          className="mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
                           data-te-ripple-init
                           data-te-ripple-color="light"
                           onClick={handleLogin}
-                          style={{ background: "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)" }}>
+                          style={{
+                            background:
+                              "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
+                          }}
+                        >
                           Login
                         </button>
                       </div>
@@ -240,19 +229,23 @@ function Register(props) {
                   </div>
                 </div>
                 <div
-                  class="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
-                  style={{ background: "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)" }}>
+                  className="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
+                  style={{
+                    background:
+                      "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
+                  }}
+                >
                   <img
-                    class="object-cover h-full w-full"
+                    className="object-cover h-full w-full"
                     src="https://c4.wallpaperflare.com/wallpaper/923/727/796/anime-digital-art-artwork-2d-portrait-display-hd-wallpaper-preview.jpg"
-                    alt="logo" />
+                    alt="logo"
+                  />
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
     </section>
   );
 }
