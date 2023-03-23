2eaa1edde372b4cbb02ae0a6d50fc03bc467639c
RJ <randy.j416@yahoo.com>
2023-03-23 17:23:45 -0400

    fix likes-adding

--- nerds-dating/src/App.js
+++ nerds-dating/src/App.js
@@ -13,23 +13,9 @@ import SideBar from "./components/SideBar/SideBar";
 
 function App() {
   const [display, setDisplay] = useState(1);
-  const testUser = {
-    id: 1,
-    name: "Jason",
-    email: "testing@gmail.com",
-    phone_number: "111-111-1111",
-    profile_picture:
-      "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__340.jpg",
-    cover_picture:
-      "https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80",
-    gender_identity: "M",
-    gender_preference: "M",
-    summary: "testing",
-    password: "password",
-  };
   const [userID, setUserID] = useState(0);
-  const { potentialMatchList, setPotentialMatchList, next } = useCurrentUserMatches(1);
-  const { profile, setProfile} = useProfile(3)
+  const { potentialMatchList, setPotentialMatchList, next } = useCurrentUserMatches(userID);
+  const { profile, setProfile} = useProfile(userID)
 
 
   return (
@@ -42,14 +28,15 @@ function App() {
         <button onClick={()=>lookUpUser(3).then(res=> setProfile(res))}>Right!</button>
         <button onClick={()=>display === 66? setDisplay(77) : setDisplay(66)}>switch!</button>
         <button onClick={()=>setPotentialMatchList([])}>empty matches!</button>
+              <button className="flex px-64" onClick={()=>display === 6? setDisplay(77) : setDisplay(6)}>switch!</button>
       </div> */}
       <ToastContainer />
-      {display === 66 && <Match
+      {display === 6 && <Match
         potentialMatches={potentialMatchList}
         discard={next}
+        currentUser={userID}
       />}
-      {display === 77 && <Profile currentUser={profile} />}
-      {display === 55 && <Profile currentUser={testUser} />}
+      {/* {display === 77 && <Profile currentUserID={userID} />} */}
       {userID !== 0 && <SideBar setDisplay={setDisplay} userID={userID} />}
       {display === 1 && (
         <Login
--- nerds-dating/src/components/Profile/Profile.jsx
+++ nerds-dating/src/components/Profile/Profile.jsx
@@ -1,22 +1,45 @@
+import lookUpUser from "../../helpers/lookUpUser"
+
 export default function Profile(props) {
 
-  const user = props.currentUser;
+  const user = lookUpUser(props.currentUserID);
 
-  return (
+  const userMapper = (userObj) => {
+    return `
     <div className="flex h-screen bg-emerald-200 pl-16">
       <>
       <div className="left-0 flex h-screen w-1/2 justify-center items-center">
         <img
           className="object-scale-down w-2/3 h-2/3 p-2"
-          src={user?.cover_picture}
+          src={lookUpUser(props.currentUserID)?.cover_picture}
           alt="Cover Pic"
         />
       </div>
         <div className="flex flex-col  w-1/2 gap-2 py-16 pr-16">
-          <div className="flex bg-fuchsia-200 rounded-3xl justify-center text-6xl">{user?.name}</div>
-          <div className="flex bg-fuchsia-200 grow rounded-3xl justify-center text-3xl">{user?.summary}</div>
+          <div className="flex bg-fuchsia-200 rounded-3xl justify-center text-6xl">{lookUpUser(props.currentUserID)?.name}</div>
+          <div className="flex bg-fuchsia-200 grow rounded-3xl justify-center text-3xl">{lookUpUser(props.currentUserID)?.summary}</div>
         </div>
       </>
     </div>
+    `
+  }
+
+  return (
+    userMapper(lookUpUser(props.currentUserID))
+    // <div className="flex h-screen bg-emerald-200 pl-16">
+    //   <>
+    //   <div className="left-0 flex h-screen w-1/2 justify-center items-center">
+    //     <img
+    //       className="object-scale-down w-2/3 h-2/3 p-2"
+    //       src={lookUpUser(props.currentUserID)?.cover_picture}
+    //       alt="Cover Pic"
+    //     />
+    //   </div>
+    //     <div className="flex flex-col  w-1/2 gap-2 py-16 pr-16">
+    //       <div className="flex bg-fuchsia-200 rounded-3xl justify-center text-6xl">{lookUpUser(props.currentUserID)?.name}</div>
+    //       <div className="flex bg-fuchsia-200 grow rounded-3xl justify-center text-3xl">{lookUpUser(props.currentUserID)?.summary}</div>
+    //     </div>
+    //   </>
+    // </div>
   );
 }
